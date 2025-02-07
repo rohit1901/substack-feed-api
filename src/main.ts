d55bef49ab335605a6bab69cd556d7f45ad39158
Rohit Khanduri <rohit.khanduri@hotmail.com>
2025-02-07 18:03:53 +0100

    feat: updated getSubstackFeedByLink method signature
    now returns raw XML string when no callback is passed

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
+import { getFeedByLink, getPosts, getSubstackFeed } from "../lib/main.ts";
+import viteLogo from "../public/vite.svg";
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
-import viteLogo from "../public/vite.svg";
-import { getSubstackFeed, getFeedByLink, getPosts } from "../lib/main.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h2>An API which fetches articles from Substack</h2>
     <div>
-    <p>Built with 
+    <p>Built with
         <img src="${typescriptLogo}" alt="Typescript Logo" width="20" height="20" />
         <img src="${viteLogo}" alt="Vite Logo" width="20" height="20" />
     </p>
-    
+
 </div>
     <p> This page displays a list of articles from <code>https://rohitkhanduri.substack.com</code> </p>
   </div>
 `;
 const SUBSTACK_FEED_URL = "https://rohitkhanduri.substack.com";
-getSubstackFeed(`${SUBSTACK_FEED_URL}/feed`, (err, rawRes) => {
+getSubstackFeed(`${SUBSTACK_FEED_URL}/feed`, true, (err, rawRes) => {
   if (err) throw err;
   const feedChannel = getFeedByLink(rawRes, SUBSTACK_FEED_URL);
   const posts = getPosts(feedChannel);
